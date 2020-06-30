dcb26bba41a3164460e6b1affb200c368c23dc0e
Jan Horacek <horacj10@fit.cvut.cz>
2020-06-30 19:53:46 +0200

    Change agent to movies

--- CSFDAgent.bundle/Contents/Code/__init__.py
+++ CSFDAgent.bundle/Contents/Code/__init__.py
@@ -7,7 +7,7 @@ def Start():
         'User-Agent'] = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)'
 
 
-class CSFDAgent(Agent.TV_Shows):
+class CSFDAgent(Agent.Movies):
     name = 'CSFD Agent'
     languages = [Locale.Language.English]
     primary_provider = True
